29a3d9d6f52fb1f969dadddc94ed69a6552c30e1
artemodin1 <51001062+artemodin1@users.noreply.github.com>
2020-10-14 16:31:29 +0300

    Add files via upload
    Не прошло и 300 лет, я загружаю код Hello World на Desktop

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import kivy
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.label import Label
 
 
-class TestApp(App):
+kivy.require('1.11.1')
+
+
+class MyFirstKivyApp(App):
+
     def build(self):
-        return Button(text='Test App')
+        return Label(text="Hello World !")
 
 
-if __name__ == '__main__':
-    TestApp().run()
+MyFirstKivyApp().run()
